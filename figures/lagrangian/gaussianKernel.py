35ab402a24f297cd84d2046869eb4000d2ee4dd1
Lento Manickathan <lento.manickathan@gmail.com>
2014-03-19 19:29:41 +0100

    spatial

--- figures/lagrangian/gaussianKernel.py
+++ figures/lagrangian/gaussianKernel.py
@@ -1,8 +1,25 @@
 import numpy as np
 import pylab as py
 py.ion()
-py.rc('text', usetex=True)
-py.rc('font', family='sans-serif')
+
+#-----------------------------------------------------------------------
+
+fig_width_pt = 246.0 # Get this from LaTeX using \showthe\columnwidth
+inches_per_pt = 1.0/72.27               # Convert pt to inch
+golden_mean = (np.sqrt(5)-1.0)/2.0         # Aesthetic ratio
+fig_width = fig_width_pt*inches_per_pt  # width in inches
+fig_height = fig_width*golden_mean      # height in inches
+fig_size =  [fig_width,fig_height]
+params = {'backend': 'ps',
+          'axes.labelsize': 10,
+          'text.fontsize': 10,
+          'legend.fontsize': 10,
+          'xtick.labelsize': 8,
+          'ytick.labelsize': 8,
+          'text.usetex': True,
+          'figure.figsize': fig_size,
+	  'font.family': 'sans-serif'}	
+py.rcParams.update(params)
 
 #-----------------------------------------------------------------------
 # Gaussian kernel
@@ -11,10 +28,12 @@ k = 2.0
 x = np.linspace(-5,5,100)
 z = (1/(k*np.pi*sigma**2))*np.exp(-(x**2)/(k*sigma**2))
 
+py.figure(1)
+py.axes([0.15,0.2,0.95-0.15,0.95-0.2])
 py.plot(x,z,'k')
-py.xlabel(r'$\left|\mathbf{x}\right|$')
-py.ylabel(r'$\zeta$')
 py.axis([-5,5,0,0.2])
+py.xlabel(r'$\left|\mathbf{x}\right|$')
+py.ylabel(r'$\zeta_{\sigma}$')
 py.grid(True)
 py.savefig('gaussianKernel.pdf')
 
